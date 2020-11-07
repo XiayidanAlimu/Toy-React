8562fafc4cb25e51788938d6fec439d2c9ca48ad
Xiayidan ALIMU <xau1@3ds.com>
2020-11-07 18:21:37 +0100

    Tic tac toe game realization on Toy React

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Toy-React
+
+### 环境搭建
+npm, node
+
+### 代码打包运行
+npx webpack
+
+#### 学习目标 
+
+1. 基于实 DOM 体系的 toy-react 的 component 的设定,实现自定义组件
+2. 给 toy-react 添加state, 对应的生命周期，实现动态修改内容的功能
+
--- toy-react.js
+++ toy-react.js
@@ -12,7 +12,11 @@ class ElementWrapper {
     if(name.match(/^on([\s\S]+)/)){
       this.root.addEventListener(RegExp.$1.replace(/^[\s\S]/, c=>c.toLowerCase()), value)
     } else {
-      this.root.setAttribute(name, value)
+      if(name === "className") {
+        this.root.setAttribute("class", value)
+      } else {
+        this.root.setAttribute(name, value)
+      }
     }
   }
 
@@ -78,8 +82,17 @@ export class Component {
   // 重新绘制ranged的函数
   rerender()
   {
-    this._range.deleteContents()
-    this[RENDER_TO_DOM](this._range)
+    let oldRange = this._range
+
+    //创建一个要插入的range,放在老的range的之前
+    let range = document.createRange()
+    range.setStart(oldRange.startContainer, oldRange.startOffset)
+    range.setEnd(oldRange.startContainer, oldRange.startOffset)    
+    this[RENDER_TO_DOM](range)
+
+    // 把老的range的start设置在新的range
+    oldRange.setStart(range.endContainer, range.endOffset)
+    oldRange.deleteContents()
   }
 
   setState(newState)
@@ -125,6 +138,9 @@ export function createElement(type, attributes, ...children) {
     for( let child of children ) {
       if( typeof child === "string" ) {
         child = new TextWrapper(child)
+      }
+      if( child === null ) {
+        continue
       }
       if( (typeof child === "object" && child instanceof Array) ) {
         insertChildren(child)
